1ac22970b2ddbdf0c760974dd1e075baf77ed425
Robtura <Robertas.Turauskas@gmail.com>
2023-01-15 15:43:31 +0200

    Edited

--- NotificationSystemDanske/NotificationSystemDanske/Services/CompanyService/CompnayService.cs
+++ NotificationSystemDanske/NotificationSystemDanske/Services/CompanyService/CompnayService.cs
@@ -12,13 +12,7 @@ public class CompnayService : ICompanyService
     public CompnayService(DataContext context)
     {
         _context = context;
-    }
-
-   /* public async Task<List<Entity.NotificationSchedule>> GetCompanyNotification(Guid entity_id)
-    {
-        var schedule = await _context.NotificationSchedule.Where(x => x.Company_id/*.ToString()// == entity_id).ToListAsync();
-        return schedule;
-    }*/
+    
 
     public async Task<List<NotificationSystemDanske.Models.NotificationSchedule>> GetCompanyNotification(Guid id)
     {
@@ -41,11 +35,6 @@ public class CompnayService : ICompanyService
 
         if (company.Market.Equals("Denmark"))
         {
-            /* _context.NotificationSchedule.Add(new NotificationSchedule(company.Entity_Id, current.AddDays(1).ToString("dd/MM/yyyy")));
-             _context.NotificationSchedule.Add(new NotificationSchedule(company.Entity_Id, current.AddDays(5).ToString("dd/MM/yyyy")));
-             _context.NotificationSchedule.Add(new NotificationSchedule(company.Entity_Id, current.AddDays(10).ToString("dd/MM/yyyy")));
-             _context.NotificationSchedule.Add(new NotificationSchedule(company.Entity_Id, current.AddDays(15).ToString("dd/MM/yyyy")));
-             _context.NotificationSchedule.Add(new NotificationSchedule(company.Entity_Id, current.AddDays(20).ToString("dd/MM/yyyy")));*/
 
             _context.NotificationSchedule.Add(new Entity.NotificationSchedule(company.Id, current.AddDays(1)));
             _context.NotificationSchedule.Add(new Entity.NotificationSchedule(company.Id, current.AddDays(5)));
@@ -56,11 +45,6 @@ public class CompnayService : ICompanyService
 
         if (company.Market.Equals("Norway"))
         {
-            /*_context.NotificationSchedule.Add(new NotificationSchedule(company.Entity_Id, current.AddDays(1).ToString("dd/MM/yyyy")));
-            _context.NotificationSchedule.Add(new NotificationSchedule(company.Entity_Id, current.AddDays(5).ToString("dd/MM/yyyy")));
-            _context.NotificationSchedule.Add(new NotificationSchedule(company.Entity_Id, current.AddDays(10).ToString("dd/MM/yyyy")));
-            _context.NotificationSchedule.Add(new NotificationSchedule(company.Entity_Id, current.AddDays(20).ToString("dd/MM/yyyy")));*/
-
             _context.NotificationSchedule.Add(new NotificationSchedule(company.Id, current.AddDays(1)));
             _context.NotificationSchedule.Add(new NotificationSchedule(company.Id, current.AddDays(5)));
             _context.NotificationSchedule.Add(new NotificationSchedule(company.Id, current.AddDays(10)));
@@ -69,11 +53,6 @@ public class CompnayService : ICompanyService
 
         if (company.Market.Equals("Sweden") && company.Type.Equals("small") || company.Type.Equals("medium"))
         {
-            /*_context.NotificationSchedule.Add(new NotificationSchedule(company.Entity_Id, current.AddDays(1).ToString("dd/MM/yyyy")));
-            _context.NotificationSchedule.Add(new NotificationSchedule(company.Entity_Id, current.AddDays(7).ToString("dd/MM/yyyy")));
-            _context.NotificationSchedule.Add(new NotificationSchedule(company.Entity_Id, current.AddDays(14).ToString("dd/MM/yyyy")));
-            _context.NotificationSchedule.Add(new NotificationSchedule(company.Entity_Id, current.AddDays(28).ToString("dd/MM/yyyy")));*/
-
             _context.NotificationSchedule.Add(new NotificationSchedule(company.Id, current.AddDays(1)));
             _context.NotificationSchedule.Add(new NotificationSchedule(company.Id, current.AddDays(7)));
             _context.NotificationSchedule.Add(new NotificationSchedule(company.Id, current.AddDays(14)));
@@ -82,12 +61,6 @@ public class CompnayService : ICompanyService
 
         if (company.Market.Equals("Finland") && company.Type.Equals("large"))
         {
-            /*_context.NotificationSchedule.Add(new NotificationSchedule(company.Entity_Id, current.AddDays(1).ToString("dd/MM/yyyy")));
-            _context.NotificationSchedule.Add(new NotificationSchedule(company.Entity_Id, current.AddDays(5).ToString("dd/MM/yyyy")));
-            _context.NotificationSchedule.Add(new NotificationSchedule(company.Entity_Id, current.AddDays(10).ToString("dd/MM/yyyy")));
-            _context.NotificationSchedule.Add(new NotificationSchedule(company.Entity_Id, current.AddDays(15).ToString("dd/MM/yyyy")));
-            _context.NotificationSchedule.Add(new NotificationSchedule(company.Entity_Id, current.AddDays(20).ToString("dd/MM/yyyy")));*/
-
             _context.NotificationSchedule.Add(new NotificationSchedule(company.Id, current.AddDays(1)));
             _context.NotificationSchedule.Add(new NotificationSchedule(company.Id, current.AddDays(5)));
             _context.NotificationSchedule.Add(new NotificationSchedule(company.Id, current.AddDays(10)));
